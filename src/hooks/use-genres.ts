99e8b799964c5de35c81f25bdae79b539acb1aa0
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-09-21 23:35:45 +0530

    Refactor: Genres hook with react query

--- src/hooks/use-genres.ts
+++ src/hooks/use-genres.ts
@@ -1,4 +1,8 @@
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { Response } from './use-data';
 import genres from '../data/genres';
+// import genres from '../data/genres';
 
 export interface Genre {
   id: number;
@@ -6,6 +10,18 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenre = () => ({ data: genres, isLoading: false, error: null });
+// const useGenre = () => ({ data: genres, isLoading: false, error: null });
+const useGenre = () => {
+  return useQuery({
+    queryKey: ['genres'],
+    queryFn: () =>
+      apiClient.get<Response<Genre>>('/genres').then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: {
+      count: genres.length,
+      results: genres,
+    },
+  });
+};
 
 export default useGenre;
