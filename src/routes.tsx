3f46df14b8c9dada338b2dda5085991e3ac8c7a4
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-09-23 00:09:11 +0530

    Handled error routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from './pages/Layout';
 import HomePage from './pages/HomePage';
 import GameDetailPage from './pages/GameDetailPage';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '',
