bac557867f126f904fae4b4d2fb7681339f6ee78
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-09-23 00:44:56 +0530

    Built expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,16 +1,19 @@
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/use-game';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, Spinner } from '@chakra-ui/react';
+import ExpandableText from '../components/ExpandableText';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, error, isLoading } = useGame(slug!);
+
   if (isLoading) return <Spinner></Spinner>;
   if (error || !game) throw error;
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
