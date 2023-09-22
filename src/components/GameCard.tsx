73724a8ca63a6392545ea4993752097e59841725
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-09-23 00:30:04 +0530

    Fetched game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIcons from './PlatformIcons';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
 import Emoji from './Emoji';
+import { Link } from 'react-router-dom';
 
 interface Props {
   game: Game;
@@ -23,7 +24,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
         <Heading fontSize={'2xl'}>
-          {game.name}
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
           <Emoji rating={game.rating_top}></Emoji>
         </Heading>
       </CardBody>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -23,6 +23,12 @@ class ApiClient<T> {
       .get<Response<T>>(this.endpoint, config)
       .then((res) => res.data);
   };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
 }
 
 export default ApiClient;
